09fefc1dfce2f65883c3a1d72542958e09ba6b0f
KilL4Life <76258827+jenralsvg@users.noreply.github.com>
2021-02-27 22:28:46 +0800

    Add files via upload

---
+++ src/donasi.js
@@ -0,0 +1,18 @@
+const donasi = (prefix) => {
+	return `╔════════════════════
+║ *DONASI UNTUK BOT SAVE NOMOR BOT*
+╠════════════════════
+║╭──❉ *JRL Enzet BOT* ❉─────
+║│➸ *BOT GRATIS JIKA INGIN PAKAI CHAT*
+║│➸ *Nomor Wa : 6285757196481*
+║╰──────────────────
+╠════════════════════
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║  ▌│█║▌║▌║║▌║▌║█│▌
+╠════════════════════
+║ _*POWERED BY JRL Enzet BOT*_
+╚════════════════════
+`
+}
+
+exports.donasi = donasi
---
+++ src/help.js
@@ -0,0 +1,25 @@
+const help = (prefix) => {
+	return `╔════════════════════
+║ *BERIKUT FITURE DARI BOT JRL ENZET*
+╠════════════════════
+┃╭───❉ *JRL Enzet BOT* ❉─────
+║│➸ *${prefix}IslamMenu*
+║│➸ *${prefix}OtherMenu*
+║│➸ *${prefix}GroupMenu*
+║│➸ *${prefix}MakerMenu*
+║│➸ *${prefix}SoundMenu*
+║│➸ *${prefix}HeppyMenu*
+║│➸ *${prefix}DownloadMenu*
+║│➸ *${prefix}DeveloperMenu*
+║╰──────────────────
+╠════════════════════
+║  ▌*${prefix}Info*
+║  ▌*${prefix}Owner*
+║  ▌*${prefix}Donasi*
+║  ▌*${prefix}BlockList*
+╠════════════════════
+┃  _*POWERED BY JRL Enzet BOT*_
+╚════════════════════`
+}
+
+exports.help = help
